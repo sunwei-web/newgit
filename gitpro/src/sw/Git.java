26be3eddc1eed0afdce3455c38a53cc52b2fd9f1
sw <sw1832656667@163.com>
2021-03-11 20:27:35 +0800

    创建一个方法

--- gitpro/src/sw/Git.java
+++ gitpro/src/sw/Git.java
@@ -3,6 +3,10 @@ package sw;
 
 public class Git {
     public static void main(String[] args) {
+        test();
+    }
 
+    private static void test() {
+        System.out.println(123132);
     }
 }
